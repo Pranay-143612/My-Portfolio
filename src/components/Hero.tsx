74af4245e049ba50ea3157a9cfb52eb75926f87a
pranay <pranayacharya2004@gmail.com>
2025-07-02 19:33:58 +0530

    added 3d model

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -135,7 +135,7 @@ const Hero: React.FC = () => {
             </motion.div>
           </motion.div>
 
-          {/* <motion.div
+          <motion.div
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1, delay: 0.5 }}
@@ -159,7 +159,7 @@ const Hero: React.FC = () => {
                 <AnimatedSphere />
               </Suspense>
             </Canvas>
-          </motion.div> */}
+          </motion.div>
         </div>
       </div>
 
